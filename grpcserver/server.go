82310f14c7a63ff8d0c9f2dfb353f86353ecc44e
shadowshot-x <ujjwal26599@gmail.com>
2022-12-19 13:33:24 +0530

    parallelize rpc and rest. implement ActuatorCheck

--- grpcserver/server.go
+++ grpcserver/server.go
@@ -2,13 +2,24 @@ package grpcserver
 
 import (
 	"context"
+	"fmt"
 	"shadowshot-x/actuatorbuf/protobufs"
 )
 
-type Server struct {
+type PingServer struct {
 	protobufs.UnimplementedPingRPCServer
 }
 
-func (s *Server) PingCheck(ctx context.Context, pm *protobufs.PingMessage) (*protobufs.PingResponse, error) {
+type ActuatorServer struct {
+	protobufs.UnimplementedActuatorServer
+}
+
+func (s *PingServer) PingCheck(ctx context.Context, pm *protobufs.PingMessage) (*protobufs.PingResponse, error) {
 	return &protobufs.PingResponse{Ping: "server alive"}, nil
+}
+
+func (s *ActuatorServer) ContractStateCheck(ctx context.Context, pm *protobufs.ContractVariableState) (*protobufs.ContractVariableStateCheck, error) {
+	fmt.Println(pm)
+	response := &protobufs.ContractVariableStateCheck{StateCheck: true}
+	return response, nil
 }
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"shadowshot-x/actuatorbuf/grpcserver"
 	"shadowshot-x/actuatorbuf/protobufs"
 	"shadowshot-x/actuatorbuf/rest"
+	"sync"
 
 	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -15,23 +16,9 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-func main() {
-	// GRPC ROUTER FOR CLIENT INTERACTION
-	listener, err := net.Listen("tcp", ":9091")
-	if err != nil {
-		panic(err)
-	}
-
-	s := grpc.NewServer()
-	reflection.Register(s)
-	protobufs.RegisterPingRPCServer(s, &grpcserver.Server{})
-	if err := s.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-
+func restServer() {
 	fmt.Println("gcp")
-
-	/// REST ROUTER FOR MAINTAINER INTERACTION
+	// / REST ROUTER FOR MAINTAINER INTERACTION
 	mainRouter := mux.NewRouter()
 
 	maintainerController := rest.MaintainerController{}
@@ -44,9 +31,34 @@ func main() {
 		Addr:    ":9090",
 		Handler: cors(mainRouter),
 	}
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error Booting the Server", err)
 	}
+}
+
+func grpcServer() {
+	fmt.Println("gcp")
+	// GRPC ROUTER FOR CLIENT INTERACTION
+	listener, err := net.Listen("tcp", ":9091")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	protobufs.RegisterPingRPCServer(s, &grpcserver.PingServer{})
+	protobufs.RegisterActuatorServer(s, &grpcserver.ActuatorServer{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go restServer()
+	go grpcServer()
+	wg.Wait()
 
 }
